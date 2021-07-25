6af7373bff863f1ea10adfb3d15974c8ccfc21c5
KareeMagdyy <karim67899@gmail.com>
2021-07-25 23:26:38 +0200

    More JS Loop Exerices

--- app.js
+++ app.js
@@ -661,3 +661,137 @@ for (let i = 0; i < count; i++) {
   document.write(`</div>`);
   document.write(`<hr>`);
 }
+
+
+///////////
+let start = 10;
+let end = 100;
+let exclude = 40;
+
+//Answer
+for (let i = start; i <= end; i += start){
+  if (i === exclude){
+      continue;
+  }
+  console.log(i);
+}
+// Output
+10
+20
+30
+50
+60
+70
+80
+90
+100
+
+
+let start = 10;
+let end = 0;
+let stop = 3;
+
+//Answer
+
+for  (let i = start; i >= stop ; i-- ){
+  if (i < start){
+      console.log(`${end}${i}`);
+  } else {
+      console.log(i);
+  }
+}
+// Output
+10
+09
+08
+07
+06
+05
+04
+03
+
+//////////////////////////
+let start = 1;
+let end = 6;
+let breaker = 2;
+
+//Answer
+for (let i = start; i <= end; i++){
+  console.log(i);
+  for (let k = breaker; k <= breaker+breaker; k += breaker){
+      console.log(`-- ${k}`);
+  }
+  
+}
+
+// Output
+1
+-- 2
+-- 4
+2
+-- 2
+-- 4
+3
+-- 2
+-- 4
+4
+-- 2
+-- 4
+5
+-- 2
+-- 4
+6
+-- 2
+-- 4
+
+//////////////
+
+let index = 10;
+let jump = 2;
+let end = 0;
+
+for (;;) {
+  // Write Your Code Here
+  console.log(index);
+    index -= jump;
+    if ( index < jump+jump){
+        break;
+    }
+}
+
+// Output
+10
+8
+6
+4
+
+/////////////////
+let friends = ["Ahmed", "Sayed", "Eman", "Mahmoud", "Ameer", "Osama", "Sameh"];
+let letter = "a";
+let friendNum = friends.indexOf('Sayed');
+
+for (let i = friends.indexOf('Ahmed'); i < friends.length; i++){
+    if (friends[i][friends.indexOf('Ahmed')] === letter.toUpperCase()){
+        continue;
+    }
+    console.log(`${friendNum++} => ${friends[i]}`);
+}
+
+// Output
+"1 => Sayed"
+"2 => Eman"
+"3 => Mahmoud"
+"4 => Osama"
+"5 => Sameh"
+
+///////////////////////
+let start = 0;
+let swappedName = "elZerO";
+
+for (let i = start; i<swappedName.length; i++){
+    if (swappedName[i] == swappedName[i].toUpperCase()) {
+        console.log(swappedName[i].toLowerCase());
+    }else {
+        console.log(swappedName[i].toUpperCase());
+    }
+}
