f09da068959abdd7f19d8631f07421902dcfd146
KareeMagdyy <karim67899@gmail.com>
2021-09-22 04:26:26 +0200

    .

--- app.js
+++ app.js
@@ -1029,3 +1029,18 @@ console.log(names("Osama", "Mohamed", "Ali", "Ibrahim"));
 // String [Osama], [Mohamed], [Ali], [Ibrahim] => Done !
 
 /* ================================= */
+// [1] Replace ??? In Return Statement To Get The Output
+// [2] Create The Same Function With Regular Syntax
+// [3] Use Array Inside The Arguments To Get The Output
+
+let myNumbers = [20, 50, 10, 60];
+
+// let calc = (one, two, ...nums) => one + two + parseInt(nums);
+// console.log(calc(10, myNumbers[0], myNumbers[1])); // 80
+
+function calc(one, two, ...nums) {
+  console.log(one + two + parseInt(nums));
+}
+calc(10, myNumbers[0], myNumbers[1]);
+////////////////
+
