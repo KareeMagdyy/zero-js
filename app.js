a763107465e718f98a5c03018004279953940c8b
KareeMagdyy <karim67899@gmail.com>
2021-09-22 02:48:11 +0200

    More Ex

--- app.js
+++ app.js
@@ -983,3 +983,29 @@ multiply("A", 10, 30); // 300
 multiply(100.5, 10, "B"); // 1000
 ////////////////////
 
+// user, age, rate, show, skills
+
+
+function showInfo(user = 'Unknown', age = 'Unknown', rate = 0, show = "Yes", ...skills){
+  document.write(`<div>`);
+  document.write(`<h2>Welcome: ${user}</h2>`);
+  document.write(`<p>Age: ${age}</p>`);
+  document.write(`<p>Rate: $${rate}`);
+  if (show === "Yes"){
+      if (skills.length > 0){
+          document.write(`<p>Skills: ${skills.join(", ")}</p>`);
+      }else{
+          document.write(`<p>Skills: No Skills</p>`);
+      }
+  }else{
+      if (skills.length > 0){
+          document.write(`<p>Skills: Hidden</p>`);
+      }else{
+          document.write(`<p>Skills: No Skills</p>`);
+      }
+  }
+  document.write(`</div>`);
+}
+
+showInfo('Kareem', 28, 40, 'No', 'HTML', 'CSS', 'JS');
+/////////////////////
