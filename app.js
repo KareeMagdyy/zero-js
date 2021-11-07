dd82cdf95b322d11bdd5865f25c27e9f65fc57ac
KareeMagdyy <karim67899@gmail.com>
2021-11-07 21:46:11 +0200

    Functional Programing

--- app.js
+++ app.js
@@ -1128,3 +1128,26 @@ function specialMix(...data) {
 }
 /////////////////////////
 
+let mix = [1, 2, 3, "E", 4, "l", "z", "e", "r", 5, "o"];
+let zero = mix
+  .map(function (ele) {
+    return isNaN(parseInt(ele)) ? ele : "";
+  })
+  .reduce(function (a, b) {
+    return a + b;
+  });
+
+console.log(zero);
+//////////////////////
+
+let myString = "EElllzzzzzzzeroo";
+
+let clearDubs = myString
+  .split("")
+  .filter(function (item, index, arr) {
+    return arr.indexOf(item) == index;
+  })
+  .reduce((a, b) => a + b);
+
+console.log(clearDubs);
+/////////////////////////
