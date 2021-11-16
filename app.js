4ea4f429f0c30b4495be3f2aebdbdcc0a0aade1d
KareeMagdyy <karim67899@gmail.com>
2021-11-16 12:22:41 +0200

    objects & methods

--- app.js
+++ app.js
@@ -1176,6 +1176,7 @@ function flatten(arr) {
 
 console.log(flatten(myArray).join(""));
 ///////////////
+
 let numsAndStrings = [1, 10, -10, -20, 5, "A", 3, "B", "C"];
 
 let nums = numsAndStrings
@@ -1191,3 +1192,162 @@ let sum = nums.reduce(function (acc, cur) {
 
 console.log(sum);
 /////////////
+
+let member = new Object({
+  name: "Elzero",
+  age: 38,
+  country: "Egypt",
+  fullDetails: function () {
+    return `My Name Is ${this.name}, My Age Is ${this.age}, I Live in ${this.country}`;
+  },
+});
+
+console.log(member.name); // Elzero
+console.log(member.age); // 38
+console.log(member.country); // Egypt
+console.log(member.fullDetails());
+// My Name Is Elzero, My Age Is 38, I Live in Egypt
+/////////////////
+
+// Method One
+// Create Your Object Here
+let objMethodOne = {
+  property: "Method One",
+  name: "Kareem",
+  age: 28,
+};
+
+console.log(objMethodOne.property); // "Method One"
+
+// Method Two
+// Create Your Object Here
+let objMethodTwo = new Object({
+  property: "Method Two",
+  name: "Kareem",
+  age: 28,
+});
+
+console.log(objMethodTwo.property); // "Method Two"
+
+// Method Three
+// Create Your Object Here
+let objMethodThree = Object.create(objMethodOne);
+objMethodThree.property = "Method Three";
+
+console.log(objMethodThree.property); // "Method Three"
+
+// Method Four
+// Create Your Object Here
+let objMethodFour = Object.assign(objMethodOne);
+objMethodFour.property = "Method Four";
+
+console.log(objMethodFour.property); // "Method Four"
+
+///////////////////
+
+let a = 1;
+
+let threeNums = {
+  b: 2,
+  c: 3,
+  d: 4,
+};
+
+let twoNums = {
+  e: 5,
+  f: 6,
+};
+
+// Create Your Object Here in One Line
+
+let finalObject = Object.assign({a}, threeNums, twoNums);
+
+console.log(finalObject);
+
+/*
+  a: 1
+  b: 2
+  c: 3
+  d: 4
+  e: 5
+  f: 6
+*/
+/////////////////////
+// The Object To Work With
+let myFavGames = {
+  "Trinity Universe": {
+    publisher: "NIS America",
+    price: 40,
+  },
+  "Titan Quest": {
+    publisher: "THQ",
+    bestThree: {
+      one: "Immortal Throne",
+      two: "Ragnarök",
+      three: "Atlantis",
+    },
+    price: 50,
+  },
+  YS: {
+    publisher: "Falcom",
+    bestThree: {
+      one: "Oath in Felghana",
+      two: "Ark Of Napishtim",
+      three: "origin",
+    },
+    price: 40,
+  },
+};
+
+// Code One => How To Get Object Length ?
+let objectLength = Object.keys(myFavGames).length;
+
+for (let i = 0; i < objectLength; i++) {
+  console.log(`The Game Name Is ${Object.keys(myFavGames)[i]}`);
+  console.log(
+    `The Publisher Is ${myFavGames[Object.keys(myFavGames)[i]].publisher}`
+  );
+  console.log(`The Price Is ${myFavGames[Object.keys(myFavGames)[i]].price}`);
+
+  // Check If Nested Object Has Property (bestThree)
+  if (myFavGames[Object.keys(myFavGames)[i]].hasOwnProperty("bestThree")) {
+    console.log("- Game Has Releases");
+    console.log(
+      `First => ${myFavGames[Object.keys(myFavGames)[i]].bestThree.one}`
+    );
+    console.log(
+      `Second => ${myFavGames[Object.keys(myFavGames)[i]].bestThree.two}`
+    );
+    console.log(
+      `Third => ${myFavGames[Object.keys(myFavGames)[i]].bestThree.three}`
+    );
+  }
+  console.log("#".repeat(20));
+}
+
+// Ouput
+
+// "The Game Name Is Trinity Universe"
+// "The Publisher Is NIS America"
+// "The Price Is 40"
+// "####################"
+// "The Game Name Is Titan Quest"
+// "The Publisher Is THQ"
+// "The Price Is 50"
+// "- Game Has Releases"
+// "First => Immortal Throne"
+// "Second => Ragnarök"
+// "Third => Atlantis"
+// "####################"
+// "The Game Name Is YS"
+// "The Publisher Is Falcom"
+// "The Price Is 40"
+// "- Game Has Releases"
+// "First => Oath in Felghana"
+// "Second => Ark Of Napishtim"
+// "Third => origin"
+// "####################"
+
+
+///////////////////////////
+
